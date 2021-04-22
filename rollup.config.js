6d5878c106bfb6160b7abfd5b6096f3cad0b5f16
nickqw14 <nickqw14@gmail.com>
2021-04-22 00:00:24 -0600

    feat: setup project config, using rollup and typescript

---
+++ rollup.config.js
@@ -0,0 +1,21 @@
+import typescript from "rollup-plugin-typescript2";
+import ttypescript from "ttypescript";
+
+export default {
+  input: "src/main.ts",
+  output: [
+    {
+      file: "dist/bundle_esm.js",
+      format: "esm",
+    },
+    {
+      file: "dist/bundle_cjs.js",
+      format: "cjs",
+    },
+  ],
+  plugins: [
+    typescript({
+      typescript: ttypescript,
+    }),
+  ],
+};
