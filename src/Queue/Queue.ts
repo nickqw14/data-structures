5f992550d1476e518df713147f24ed86644552b9
nickqweaver <nick.weaver@sendoutcards.com>
2021-05-12 15:05:40 -0600

    feat: added Queue data structure

---
+++ src/Queue/Queue.ts
@@ -0,0 +1,55 @@
+import { LinkedList } from '../LinkedList/LinkedList'
+
+interface QueueInterface<T> {
+    enqueue: (element: T) => void
+    dequeue: () => T
+    size: () => number
+    clear: () => void
+    peek: () => T
+}
+/**
+ * Class for Stack Data Structure
+ */
+export class Queue<E> implements QueueInterface<E> {
+    private list: LinkedList<E>
+
+    constructor() {
+        this.list = new LinkedList<E>()
+    }
+
+    /**
+     * Adds the element to the back of the Queue
+     * @param element
+     */
+    enqueue(element: E) {
+        this.list.add(element)
+    }
+
+    /**
+     * Removes & returns the first element in the Queue
+     */
+    dequeue() {
+        return this.list.removeFirst()
+    }
+
+    /**
+     * @returns the size of the Queue
+     */
+    size() {
+        return this.list.getSize()
+    }
+
+    /**
+     * Removes all items in the Queue
+     */
+    clear() {
+        this.list.clear()
+    }
+
+    /**
+     * @returns the first item in the Queue
+     */
+    peek() {
+        return this.list.getFirst()
+    }
+}
