a323d5be591f72df6bda0ceed28706c5587b2191
nickqw14 <nickqw14@gmail.com>
2021-10-29 11:46:42 -0600

    feat: v1.05

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import typescript from 'rollup-plugin-typescript2'
 import ttypescript from 'ttypescript'
 
 export default {
-    input: 'src/index.ts',
+    input: 'index.ts',
     output: [
         {
             file: 'dist/index.js',
