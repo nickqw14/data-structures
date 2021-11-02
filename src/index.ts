40ca7444cbb2976e3b4bb20dd62aa1661a03bf7a
nickqw14 <nickqw14@gmail.com>
2021-11-01 21:40:30 -0600

    fix: removed dumb code

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,3 @@
 export * from '../src/LinkedList/LinkedList'
 export * from '../src/Queue/Queue'
 export * from '../src/Stack/Stack'
-
-export const testFunction = () => {
-    console.log('Hello world')
-}
